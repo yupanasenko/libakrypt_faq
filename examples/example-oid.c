8cb1af2789e513ca896b68e92bf3c0c7bc395bb6
Axel Zalman <axelkenzo@mail.ru>
2017-09-29 18:51:54 +0300

    улучшена документация по внутреннему интерфейсу класса ak_hash

--- examples/example-oid.c
+++ examples/example-oid.c
@@ -7,7 +7,7 @@
  int main( void )
 {
   ak_handle handle = ak_error_wrong_handle;
-  const char *hash = "streebog512"; /* это имя функции хеширования */
+  const char *good = "streebog512"; /* это имя функции хеширования */
   const char *wrong = "2.16.840.1.101.3.4.2.3"; /* это OID функции SHA2-256 */
 
  /* инициализируем библиотеку */
@@ -29,9 +29,9 @@
 
  /* ищем OID по его имени */
   printf("\nsearch results:\n");
-  if(( handle = ak_oid_find_by_name( hash )) != ak_error_wrong_handle )
+  if(( handle = ak_oid_find_by_name( good )) != ak_error_wrong_handle )
      print_oid_info( handle );
-   else printf("oid with name \"%s\" not found\n", hash );
+   else printf("oid with name \"%s\" not found\n", good );
 
  /* ищем OID по его цифровому идентификатору (последовательности чисел, разделенных точками */
   if(( handle = ak_oid_find_by_id( wrong )) != ak_error_wrong_handle )
