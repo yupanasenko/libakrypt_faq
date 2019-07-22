40ffffc69f2028523d526c2b948e7b1282ee3463
Axel Kenzo <axelkenzo@mail.ru>
2019-07-22 15:50:29 +0300

    Исправлен тестовый пример tes-internal-hash04

--- tests/test-internal-hash04.c
+++ tests/test-internal-hash04.c
@@ -6,17 +6,24 @@
  #include <stdio.h>
  #include <stdlib.h>
  #include <string.h>
+ #include <unistd.h>
  #include <time.h>
  #include <ak_hash.h>
  #include <ak_parameters.h>
+#ifdef __linux__
+ #include <sys/resource.h>
+#endif
 
  int main( void )
 {
-  clock_t time;
-  int i, error, mbsize = 64;
+  clock_t timea;
+  int i, error;
   struct hash ctx;
-  ak_uint8 data[1024], out[64];
-
+  ak_uint8 *data, out[64];
+  size_t size = 0;
+  ak_int64 msec;
+  double iter = 0, avg = 0;
+  struct rusage rg1, rg2;
 
  /* инициализируем библиотеку */
   if( !ak_libakrypt_create( ak_function_log_stderr )) return ak_libakrypt_destroy();
@@ -27,18 +34,41 @@
     return ak_libakrypt_destroy();
   }
 
- /* теперь собственно зашифрование */
-  memset( data, 0x13, sizeof( data ));
-  time = clock();
-  ctx.clean( &ctx );
-  for( i = 0; i < 1024*mbsize; i++ ) {
-     ctx.update( &ctx, data, sizeof( data ));
-  }
-  ctx.finalize( &ctx, NULL, 0, out );
-  time = clock() - time;
-  printf(" hash time: %fs, per 1MB = %fs, speed = %f MBs\n",
-               (double) time / (double) CLOCKS_PER_SEC,
-               (double) time / ( (double) CLOCKS_PER_SEC * mbsize ), (double) CLOCKS_PER_SEC *mbsize / (double) time );
+  for( i = 16; i < 129; i += 16 ) {
+    data = malloc( size = ( size_t ) i*1024*1024 );
+    memset( data, 0x13, size );
 
+   /* теперь собственно хеширование памяти */
+    timea = clock();
+    getrusage( RUSAGE_SELF, &rg1 );
+    ak_hash_context_ptr( &ctx, data, size, out );
+    getrusage( RUSAGE_SELF, &rg2 );
+    timea = clock() - timea;
+    printf(" %3luMB: hash time: %fs, per 1MB = %fs, speed = %f MBs (clock)\n", (size_t)i,
+               (double) timea / (double) CLOCKS_PER_SEC,
+               (double) timea / ( (double) CLOCKS_PER_SEC*i ),
+               (double) CLOCKS_PER_SEC*i / (double) timea );
+    iter += 1;
+    avg += (double) CLOCKS_PER_SEC*i / (double) timea;
+
+#ifdef __linux__
+    msec = rg2.ru_utime.tv_sec - rg1.ru_utime.tv_sec;
+    if( rg2.ru_utime.tv_usec > rg1.ru_utime.tv_usec ) {
+      msec *= 1000000;
+      msec += (rg2.ru_utime.tv_usec - rg1.ru_utime.tv_usec);
+    } else {
+          msec *= 1000000;
+          msec -= (rg1.ru_utime.tv_usec - rg2.ru_utime.tv_usec);
+    }
+    printf(" %3luMB: hash time: %fs, per 1MB = %fs, speed = %f MBs (getrusage)\n\n", (size_t)i,
+               msec / (double) 1000000,
+               (double) timea / ( (double) 1000000*i ),
+               (double) 1000000*i / (double) timea );
+#endif
+    free( data );
+  }
+
+  printf("average memory hashing speed: %fs\n", avg/iter );
+  ak_hash_context_destroy( &ctx );
  return ak_libakrypt_destroy();
 }
