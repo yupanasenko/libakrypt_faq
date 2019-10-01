d1a443c475f205f2995affab7919277d8b65e65f
Axel Kenzo <axelkenzo@mail.ru>
2019-10-02 00:13:17 +0300

    Консольное приложение возвращается )))

---
+++ aktool/aktool.c
@@ -0,0 +1,360 @@
+/* ----------------------------------------------------------------------------------------------- */
+/*  Copyright (c) 2014 - 2019 by Axel Kenzo, axelkenzo@mail.ru                                     */
+/*                                                                                                 */
+/*  Файл akrypt.c                                                                                  */
+/*  - содержит реализацию консольной утилиты, иллюстрирующей возможности библиотеки libakrypt      */
+/* ----------------------------------------------------------------------------------------------- */
+ #include <time.h>
+ #include <stdio.h>
+ #include <string.h>
+ #include <stdlib.h>
+ #include <aktool.h>
+#ifdef _MSC_VER
+ #include <strsafe.h>
+#endif
+
+/* ----------------------------------------------------------------------------------------------- */
+ char audit_filename[1024];
+/* функция, реализующая аудит */
+ ak_function_log *audit =
+#ifdef _WIN32
+ aktool_audit_function;
+#else
+ ak_function_log_syslog;
+#endif
+
+/* ----------------------------------------------------------------------------------------------- */
+ int main( int argc, TCHAR *argv[] )
+{
+ /* определение переменных, используемых ддя указания времени старта программы */
+ #ifdef _WIN32
+  time_t ptime;
+  TCHAR homepath[FILENAME_MAX]
+   #ifdef MSC_VER
+     , buffer[64]
+   #endif
+   ;
+ #endif
+ /* попытка русификации программы для unix-like операционных систем */
+ #ifdef LIBaktool_HAVE_LIBINTL_H
+ /* обрабатываем настройки локали
+    при инсталляции файл akrypt.mo должен помещаться в /usr/share/locale/ru/LC_MESSAGES */
+  #ifdef LIBaktool_HAVE_LOCALE_H
+   setlocale( LC_ALL, "" );
+  #endif
+  bindtextdomain( "akrypt", "/usr/share/locale/" );
+  textdomain( "akrypt" );
+ #endif
+
+ /* проверяем, что пользователем должна быть задана команда */
+  if( argc < 2 ) return aktool_litehelp();
+
+ /* проверяем флаги вывода справочной информации */
+  if( aktool_check_command( "-h", argv[1] )) return aktool_help();
+  if( aktool_check_command( "--help", argv[1] )) return aktool_help();
+  if( aktool_check_command( "/?", argv[1] )) return aktool_help();
+
+ #ifdef _WIN32
+  if( ak_libakrypt_get_home_path( homepath, FILENAME_MAX ) == ak_error_ok ) {
+    ak_snprintf( audit_filename, FILENAME_MAX, "%s\\.config\\libakrypt\\akrypt.log", homepath );
+    remove( audit_filename );
+    ak_log_set_function( audit = aktool_audit_function );
+
+   #ifdef MSC_VER
+    _time64( &ptime );
+    _tctime64_s( buffer, sizeof( buffer ), &ptime );
+    ak_snprintf( homepath, FILENAME_MAX, "%s started at %s", argv[0], buffer );
+   #else
+     ptime = time( NULL );
+     ak_snprintf( homepath, FILENAME_MAX, "%s started at %s", argv[0], ctime( &ptime ));
+   #endif
+    ak_log_set_message( homepath );
+  }
+
+  SetConsoleCP( 1251 );
+  SetConsoleOutputCP( 1251 );
+ #endif
+
+ /* выполняем команду пользователя */
+  if( aktool_check_command( "show", argv[1] )) return aktool_show( argc, argv );
+  if( aktool_check_command( "i", argv[1] )) return aktool_icode( argc, argv );
+  if( aktool_check_command( "icode", argv[1] )) return aktool_icode( argc, argv );
+
+ /* ничего не подошло, выводим сообщение об ошибке */
+  ak_log_set_function( ak_function_log_stderr );
+  ak_error_message_fmt( ak_error_undefined_function,
+                                                 __func__, _("undefined command \"%s\""), argv[1] );
+ return EXIT_FAILURE;
+}
+
+/* ----------------------------------------------------------------------------------------------- */
+/*                         реализация функций обработки команд пользователя                        */
+/* ----------------------------------------------------------------------------------------------- */
+ bool_t aktool_check_command( const char *comm, TCHAR *argv )
+{
+ size_t len = strlen( comm );
+
+  if( strlen( argv ) != len ) return ak_false;
+  if( strncmp( comm, argv, len )) return ak_false;
+ return ak_true;
+}
+
+/* ----------------------------------------------------------------------------------------------- */
+/* вывод сообщений в заданный пользователем файл, а также в стандартный демон вывода сообщений */
+ int aktool_audit_function( const char *message )
+{
+  FILE *fp = fopen( audit_filename, "a+" );
+   /* функция выводит сообщения в заданный файл */
+    if( !fp ) return ak_error_open_file;
+    fprintf( fp, "%s\n", message );
+#if defined(__unix__) || defined(__APPLE__)
+    ak_function_log_syslog( message ); /* все действия дополнительно дублируются в syslog */
+#endif
+    if( fclose(fp) == EOF ) return ak_error_access_file;
+ return ak_error_ok;
+}
+
+/* ----------------------------------------------------------------------------------------------- */
+ void aktool_set_audit( TCHAR *message )
+{
+  if( ak_ptr_is_equal( "stderr", message, 6 )) {
+       audit = ak_function_log_stderr; /* если задан stderr, то используем готовую функцию */
+  } else {
+            if( strlen( message ) > 0 ) {
+              memset( audit_filename, 0, 1024 );
+              strncpy( audit_filename, message, 1022 );
+              audit = aktool_audit_function;
+            }
+         }
+}
+
+/* ----------------------------------------------------------------------------------------------- */
+ int aktool_file_or_directory( const TCHAR *filename )
+{
+ struct stat st;
+
+  if( stat( filename, &st )) return 0;
+  if( S_ISREG( st.st_mode )) return DT_REG;
+  if( S_ISDIR( st.st_mode )) return DT_DIR;
+
+ return 0;
+}
+
+/* ----------------------------------------------------------------------------------------------- */
+/* выполнение однотипной процедуры с группой файлов */
+ int aktool_find( const char *root , const char *mask,
+                                          ak_function_find *function, ak_pointer ptr, bool_t tree )
+{
+  int error = ak_error_ok;
+
+#ifdef _WIN32
+  WIN32_FIND_DATA ffd;
+  TCHAR szDir[MAX_PATH];
+  char filename[MAX_PATH];
+  HANDLE hFind = INVALID_HANDLE_VALUE;
+  size_t rlen = 0, mlen = 0;
+
+ #ifdef _MSC_VER
+  if( FAILED( StringCchLength( root, MAX_PATH-1, &rlen )))
+    return ak_error_message_fmt( ak_error_wrong_length, __func__ ,
+                                                             "incorrect length for root variable" );
+  if( FAILED( StringCchLength( mask, MAX_PATH-1, &mlen )))
+    return ak_error_message_fmt( ak_error_wrong_length, __func__ ,
+                                                            "incorrect length for mask variable" );
+ #else
+  rlen = strlen( root ); mlen = strlen( mask );
+ #endif
+
+  if( rlen > (MAX_PATH - ( mlen + 2 )))
+    return ak_error_message_fmt( ak_error_wrong_length, __func__ , "directory path too long" );
+
+ #ifdef _MSC_VER
+  if( FAILED( StringCchCopy( szDir, MAX_PATH-1, root )))
+    return ak_error_message_fmt( ak_error_wrong_length, __func__ ,
+                                                            "incorrect copying of root variable" );
+  if( FAILED( StringCchCat( szDir, MAX_PATH-1, TEXT( "\\" ))))
+    return ak_error_message_fmt( ak_error_wrong_length, __func__ ,
+                                                      "incorrect copying of directory separator" );
+  if( FAILED( StringCchCat( szDir, MAX_PATH-1, mask )))
+    return ak_error_message_fmt( ak_error_wrong_length, __func__ ,
+                                                            "incorrect copying of mask variable" );
+ #else
+  ak_snprintf( szDir, MAX_PATH-1, "%s\\%s", root, mask );
+ #endif
+
+ /* начинаем поиск */
+  if(( hFind = FindFirstFile( szDir, &ffd )) == INVALID_HANDLE_VALUE )
+    return ak_error_message_fmt( ak_error_access_file, __func__ , "given mask search error" );
+
+  do {
+       if( ffd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY ) {
+
+         if( !strcmp( ffd.cFileName, "." )) continue;  // пропускаем себя и каталог верхнего уровня
+         if( !strcmp( ffd.cFileName, ".." )) continue;
+
+         if( tree ) { // выполняем рекурсию для вложенных каталогов
+           memset( szDir, 0, MAX_PATH );
+          #ifdef _MSC_VER
+           if( FAILED( StringCchCopy( szDir, MAX_PATH-1, root )))
+             return ak_error_message_fmt( ak_error_wrong_length, __func__ ,
+                                                            "incorrect copying of root variable" );
+           if( FAILED( StringCchCat( szDir, MAX_PATH-1, TEXT( "\\" ))))
+             return ak_error_message_fmt( ak_error_wrong_length, __func__ ,
+                                                      "incorrect copying of directory separator" );
+           if( FAILED( StringCchCat( szDir, MAX_PATH-1,  ffd.cFileName )))
+             return ak_error_message_fmt( ak_error_wrong_length, __func__ ,
+                                                                "incorrect copying of file name" );
+          #else
+           ak_snprintf( szDir, MAX_PATH-1, "%s\\%s", root,  ffd.cFileName );
+          #endif
+
+           if(( error = aktool_find( szDir, mask, function, ptr, tree )) != ak_error_ok )
+             ak_error_message_fmt( error,
+                                         __func__, "access to \"%s\" directory denied", filename );
+         }
+       } else {
+               if( ffd.dwFileAttributes &FILE_ATTRIBUTE_SYSTEM ) continue;
+                 memset( filename, 0, FILENAME_MAX );
+                #ifdef _MSC_VER
+                 if( FAILED( StringCchCopy( filename, MAX_PATH-1, root )))
+                   return ak_error_message_fmt( ak_error_wrong_length, __func__ ,
+                                                            "incorrect copying of root variable" );
+                 if( FAILED( StringCchCat( filename, MAX_PATH-1, TEXT( "\\" ))))
+                   return ak_error_message_fmt( ak_error_wrong_length, __func__ ,
+                                                      "incorrect copying of directory separator" );
+                 if( FAILED( StringCchCat( filename, MAX_PATH-1,  ffd.cFileName )))
+                   return ak_error_message_fmt( ak_error_wrong_length, __func__ ,
+                                                               "incorrect copying of file namme" );
+                #else
+                 ak_snprintf( filename, MAX_PATH-1, "%s\\%s", root,  ffd.cFileName );
+                #endif
+                 function( filename, ptr );
+              }
+
+  } while( FindNextFile( hFind, &ffd ) != 0);
+  FindClose(hFind);
+
+// далее используем механизм функций open/readdir + fnmatch
+#else
+  DIR *dp = NULL;
+  struct dirent *ent = NULL;
+  char filename[FILENAME_MAX];
+
+ /* открываем каталог */
+  errno = 0;
+  if(( dp = opendir( root )) == NULL ) {
+    if( errno == EACCES ) return ak_error_message_fmt( ak_error_access_file,
+                                          __func__ , _("access to \"%s\" directory denied"), root );
+    if( errno > -1 ) return ak_error_message_fmt( ak_error_open_file,
+                                                                __func__ , "%s", strerror( errno ));
+  }
+
+ /* перебираем все файлы и каталоги */
+  while(( ent = readdir( dp )) != NULL ) {
+    if( ent->d_type == DT_DIR ) {
+      if( !strcmp( ent->d_name, "." )) continue;  // пропускаем себя и каталог верхнего уровня
+      if( !strcmp( ent->d_name, ".." )) continue;
+
+      if( tree ) { // выполняем рекурсию для вложенных каталогов
+        memset( filename, 0, FILENAME_MAX );
+        ak_snprintf( filename, FILENAME_MAX, "%s/%s", root, ent->d_name );
+        if(( error = aktool_find( filename, mask, function, ptr, tree )) != ak_error_ok )
+          ak_error_message_fmt( error, __func__, _("access to \"%s\" directory denied"), filename );
+      }
+    } else
+       if( ent->d_type == DT_REG ) { // обрабатываем только обычные файлы
+          if( !fnmatch( mask, ent->d_name, FNM_PATHNAME )) {
+            memset( filename, 0, FILENAME_MAX );
+            ak_snprintf( filename, FILENAME_MAX, "%s/%s", root, ent->d_name );
+            function( filename, ptr );
+          }
+       }
+  }
+  if( closedir( dp )) return ak_error_message_fmt( ak_error_close_file,
+                                                                __func__ , "%s", strerror( errno ));
+#endif
+ return ak_error_ok;
+}
+
+/* ----------------------------------------------------------------------------------------------- */
+ int ak_file_read_by_lines( const char *filename, ak_file_read_function *function , ak_pointer ptr )
+{
+  #define buffer_length ( FILENAME_MAX + 160 )
+
+  char ch;
+  struct stat st;
+  size_t idx = 0, off = 0;
+  int fd = 0, error = ak_error_ok;
+  char localbuffer[buffer_length];
+
+ /* проверяем наличие файла и прав доступа к нему */
+  if(( fd = open( filename, O_RDONLY | O_BINARY )) < 0 )
+    return ak_error_message_fmt( ak_error_open_file,
+                             __func__, "wrong open file \"%s\" - %s", filename, strerror( errno ));
+  if( fstat( fd, &st ) ) {
+    close( fd );
+    return ak_error_message_fmt( ak_error_access_file, __func__ ,
+                              "wrong stat file \"%s\" with error %s", filename, strerror( errno ));
+  }
+
+ /* нарезаем входные на строки длиной не более чем buffer_length - 2 символа */
+  memset( localbuffer, 0, buffer_length );
+  for( idx = 0; idx < (size_t) st.st_size; idx++ ) {
+     if( read( fd, &ch, 1 ) != 1 ) {
+       close(fd);
+       return ak_error_message_fmt( ak_error_read_data, __func__ ,
+                                                                "unexpected end of %s", filename );
+     }
+     if( off > buffer_length - 2 ) {
+       close( fd );
+       return ak_error_message_fmt( ak_error_read_data, __func__ ,
+                          "%s has a line with more than %d symbols", filename, buffer_length - 2 );
+     }
+    if( ch == '\n' ) {
+      #ifdef _WIN32
+       if( off ) localbuffer[off-1] = 0;  /* удаляем второй символ перехода на новую строку */
+      #endif
+      error = function( localbuffer, ptr );
+     /* далее мы очищаем строку независимо от ее содержимого */
+      off = 0;
+      memset( localbuffer, 0, buffer_length );
+    } else localbuffer[off++] = ch;
+   /* выходим из цикла если процедура проверки нарушена */
+    if( error != ak_error_ok ) return error;
+  }
+
+  close( fd );
+ return error;
+}
+
+/* ----------------------------------------------------------------------------------------------- */
+/*                                 реализация вывода справки                                       */
+/* ----------------------------------------------------------------------------------------------- */
+ int aktool_litehelp( void )
+{
+  printf(_("aktool (crypto application based on libakrypt library, version: %s)\n\n"),
+                                                                          ak_libakrypt_version( ));
+  printf(_("try \"aktool --help\" to get more information\n"));
+
+ return EXIT_SUCCESS;
+}
+
+/* ----------------------------------------------------------------------------------------------- */
+ int aktool_help( void )
+{
+  printf(_("aktool (crypto application based on libakrypt library, version: %s)\n"),
+                                                                          ak_libakrypt_version( ));
+  printf(_("usage \"aktool command [options] [files]\"\n\n"));
+  printf(_("available commands (in short and long forms):\n"));
+  printf(_("  i  icode  calculate or check integrity codes\n"));
+  printf(_("     show   show useful information\n\n"));
+  printf(_("also try:\n"));
+  printf(_("  \"aktool command --help\" to get information about command options\n"));
+  printf(_("  \"man aktool\" to get more information about akrypt programm and some examples\n"));
+
+ return EXIT_SUCCESS;
+}
+
+/* ----------------------------------------------------------------------------------------------- */
+/*                                                                                       akrypt.c  */
+/* ----------------------------------------------------------------------------------------------- */
---
+++ aktool/aktool_show.c
@@ -0,0 +1,204 @@
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <string.h>
+ #include <aktool.h>
+
+/* ----------------------------------------------------------------------------------------------- */
+ int aktool_show_help( void );
+
+/* ----------------------------------------------------------------------------------------------- */
+ int aktool_show( int argc, TCHAR *argv[] )
+{
+  size_t idx = 0;
+  int next_option = 0, show_caption = ak_true;
+  enum { do_nothing, do_alloids, do_oid, do_engines, do_modes, do_options } work = do_nothing;
+
+ /* параметры, запрашиваемые пользователем */
+  char *value = NULL;
+  oid_modes_t mode = 0;
+  oid_engines_t engine = 0;
+  const char *oid, **names;
+
+//  char algorithmName[128], algorithmOID[128];
+
+  const struct option long_options[] = {
+     { "oids",             0, NULL,  254 },
+     { "oid",              1, NULL,  253 },
+     { "engines",          0, NULL,  252 },
+     { "options",          0, NULL,  251 },
+     { "without-caption",  0, NULL,  250 },
+     { "modes",            0, NULL,  249 },
+
+     { "audit",            1, NULL,   2  },
+     { "help",             0, NULL,   1  },
+     { NULL,               0, NULL,   0  }
+  };
+
+ /* разбираем опции командной строки */
+  do {
+       next_option = getopt_long( argc, argv, "", long_options, NULL );
+       switch( next_option )
+      {
+         case  1  : return aktool_show_help();
+         case  2  : /* получили от пользователя имя файла для вывода аудита */
+                     aktool_set_audit( optarg );
+                     break;
+
+         case 254 : /* выводим список всех доступных oid */
+                     work = do_alloids;
+                     break;
+
+         case 253 : /* производим поиск OID по параметрам */
+                     work = do_oid; value = optarg;
+                     break;
+
+         case 252 : /* выводим список всех типов криптографических механизмов */
+                     work = do_engines;
+                     break;
+         case 251 : /* выводим список всех опций библиотеки и их значений */
+                     work = do_options;
+                     break;
+         case 250:  /* запрещаем выводить заголовок */
+                     show_caption = ak_false;
+                     break;
+         case 249:   work = do_modes;
+                     break;
+
+         default:   /* обрабатываем ошибочные параметры */
+                     if( next_option != -1 ) work = do_nothing;
+                     break;
+       }
+   } while( next_option != -1 );
+   if( work == do_nothing ) return aktool_show_help();
+
+ /* начинаем работу с криптографическими примитивами */
+   if( ak_libakrypt_create( audit ) != ak_true ) return ak_libakrypt_destroy();
+
+ /* выбираем заданное пользователем действие */
+    switch( work )
+   {
+     case do_alloids: /* выводим список всех доступных oid */
+       if( show_caption ) {
+         printf("  N  %-22s %-40s %-20s %-20s\n", _("oid"), _("name(s)"), _("engine"), _("mode") );
+         printf(" -----------------------------------------------------");
+         printf("------------------------------------------------------\n");
+       }
+
+       for( idx = 0; idx < ak_libakrypt_oids_count(); idx++ ) {
+          size_t jdx = 0;
+         /* получаем информацию об идентифкаторе с заданным номером */
+          if(( ak_libakrypt_get_oid_by_index( idx, &engine, &mode,
+                                                            &oid, &names )) != ak_error_ok ) break;
+          if( names[0] == NULL ) break;
+
+         /* выводим сначала с одним именем  */
+          printf("%3u  %-22s %-40s %-20s %-20s\n",
+            (unsigned int) idx, oid, names[0], ak_libakrypt_get_engine_name( engine ),
+                                                               ak_libakrypt_get_mode_name( mode ));
+         /* потом выводим остальные имена идентификатора */
+          while( names[++jdx] != NULL ) printf("%28s%s\n", " ", names[jdx] );
+       }
+       break;
+
+     case do_oid: /* выводим список тех, кто подходит под заданный шаблон */
+       if( show_caption ) {
+         printf("  N  %-22s %-40s %-20s %-20s\n", _("oid"), _("name(s)"), _("engine"), _("mode") );
+         printf(" -----------------------------------------------------");
+         printf("------------------------------------------------------\n");
+       }
+
+       for( idx = 0; idx < ak_libakrypt_oids_count(); idx++ ) {
+          size_t jdx = 0;
+         /* получаем информацию об идентифкаторе с заданным номером */
+          if(( ak_libakrypt_get_oid_by_index( idx, &engine, &mode,
+                                                            &oid, &names )) != ak_error_ok ) break;
+          if( names[0] == NULL ) break;
+
+         /* проверяем тип криптопреобразования (engine) */
+          if( strstr( ak_libakrypt_get_engine_name(engine), value ) != NULL ) {
+            /* выводим первое имя */
+             printf("%3u  %-22s %-40s %-20s %-20s\n",
+               (unsigned int) idx, oid, names[0], ak_libakrypt_get_engine_name( engine ),
+                                                               ak_libakrypt_get_mode_name( mode ));
+            /* потом все остальные */
+             while( names[++jdx] != NULL ) printf("%28s%s\n", " ", names[jdx] );
+            continue;
+          }
+
+         /* проверяем идентификатор (oid) */
+          if( strstr( oid, value ) != NULL ) {
+            /* выводим первое имя */
+             printf("%3u  %-22s %-40s %-20s %-20s\n",
+               (unsigned int) idx, oid, names[0], ak_libakrypt_get_engine_name( engine ),
+                                                               ak_libakrypt_get_mode_name( mode ));
+            /* потом все остальные */
+             while( names[++jdx] != NULL ) printf("%28s%s\n", " ", names[jdx] );
+            continue;
+          }
+
+         /* последнее - поиск по имени */
+          jdx = 0;
+          while( names[jdx] != NULL ) {
+            if( strstr( names[jdx], value ) != NULL ) {
+             printf("%3u  %-22s %-40s %-20s %-20s\n",
+               (unsigned int) idx, oid, names[jdx], ak_libakrypt_get_engine_name( engine ),
+                                                               ak_libakrypt_get_mode_name( mode ));
+            }
+            jdx++;
+          }
+
+       } /* конец for */
+       break;
+
+     case do_options:
+       if( show_caption ) {
+         printf(" %-40s %-16s\n", _("option"), _("value"));
+         printf("------------------------------------------------------\n");
+       }
+       for( idx = 0; idx < ak_libakrypt_options_count(); idx++ )
+          printf(" %-40s %-16ld\n", ak_libakrypt_get_option_name( idx ),
+                                                  (long int) ak_libakrypt_get_option_value( idx ));
+       break;
+
+     case do_engines:
+       if( show_caption )
+         printf(" %s\n------------------------------------------------------\n", _("engine"));
+       do {
+           printf(" %s\n", ak_libakrypt_get_engine_name( engine ));
+       } while( engine++ < undefined_engine );
+       break;
+
+     case do_modes:
+       if( show_caption )
+         printf(" %s\n------------------------------------------------------\n", _("mode"));
+       do {
+            printf(" %s\n", ak_libakrypt_get_mode_name( mode ));
+       } while( mode++ < undefined_mode );
+       break;
+
+     default:  break;
+   }
+
+ /* завершаем работу и выходим */
+ return ak_libakrypt_destroy();
+}
+
+/* ----------------------------------------------------------------------------------------------- */
+ int aktool_show_help( void )
+{
+  printf(_("aktool show [options]  - show useful information about libakrypt parameters\n\n"));
+  printf(_("available options:\n"));
+  printf(_("     --engines           show all types of available crypto engines\n"));
+  printf(_("     --oid <eni>         show one or more OID's, where \"eni\" is an engine, name or identifier of OID\n"));
+  printf(_("     --oids              show the list of all available libakrypt's OIDs\n"));
+  printf(_("     --options           show the list of all libakrypt's cryptographic options and their values\n"));
+  printf(_("     --modes             show all types of cryptographic modes\n"));
+  printf(_("     --without-caption   don't show a caption for displayed values\n"));
+
+  printf(_("\ncommon aktool options:\n"));
+  printf(_("     --audit <file>      set the output file for errors and libakrypt audit system messages\n" ));
+  printf(_("     --help              show this information\n\n"));
+
+ return EXIT_SUCCESS;
+}
+
