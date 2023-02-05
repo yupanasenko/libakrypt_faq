345ad3ecb5b8b1d813b311ee083625eabb3767a5
oabelova <123364383+oabelova@users.noreply.github.com>
2023-02-05 20:39:33 +0300

    Update example-31.c

--- examples/faq/example-31.c
+++ examples/faq/example-31.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <libakrypt-base.h>
 #include <libakrypt.h>
+
+/*Пример вычисления 32-битной контрольной суммы с помощью алгоритма Флетчера*/
 int main(){
     ak_const_pointer data = "abcde";
     ak_uint32 *res;
