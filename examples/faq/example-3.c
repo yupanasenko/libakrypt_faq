6822a5d8bdd1837b4b62b3fd26be3a9c917d9884
mafesenko <121544544+mafesenko@users.noreply.github.com>
2023-02-07 01:56:32 +0300

    Update example-3.c

--- examples/faq/example-3.c
+++ examples/faq/example-3.c
@@ -1,8 +1,16 @@
+/* ----------------------------------------------------------------------------------- */
+/* Пример example-3.c                                                             */
+/* ----------------------------------------------------------------------------------- */
 #include <stdio.h>
 #include <libakrypt.h>
 #include <libakrypt-base.h>
 
 int main() {
+if( ak_libakrypt_create( NULL ) != ak_true ) {
+   /* инициализация выполнена не успешно, следовательно, выходим из программы */
+    ak_libakrypt_destroy();
+    return EXIT_FAILURE;
+  }
     /*Структура для хранения контекста ключа*/
     struct bckey key;
 
@@ -49,4 +57,7 @@ int main() {
     		sizeof(testkey)    /*Длина синхропосылки в байтах*/
     		))
         return ak_error_get_value();
+ak_bckey_destroy(&key);
+ak_libakrypt_destroy();
+return EXIT_SUCCESS;
 }
