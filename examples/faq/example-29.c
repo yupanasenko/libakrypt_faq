3ac90eb0415a7bbce8dd2801f1be04a4aed510a0
oabelova <123364383+oabelova@users.noreply.github.com>
2023-02-07 02:12:34 +0300

    Update example-29.c

--- examples/faq/example-29.c
+++ examples/faq/example-29.c
@@ -4,26 +4,26 @@
 
 /*Пример чтения строки текста из консоли*/
 
-int main(){
-    
+int main( void )
+{
     if( ak_libakrypt_create( NULL ) != ak_true ) {
         /* инициализация выполнена не успешно, следовательно, выходим из программы */
         ak_libakrypt_destroy();
         return EXIT_FAILURE;
     }
-        char string[256];
-        size_t len = 256;
+    char string[256];
+    size_t len = 256;
 
-        if(ak_string_read("Please, enter your message: \n",string,&len)!=ak_error_ok){
-            printf("Error with reading your string!\n");
+    if(ak_string_read("Please, enter your message: \n",string,&len)!=ak_error_ok){
+        printf("Error with reading your string!\n");
+    }
+    else{
+        printf("Your string is: ");
+        int i=0;
+        for(i=0;i<len;i++){
+            printf("%c",string[i]);
         }
-        else{
-            printf("Your string is: ");
-            int i=0;
-            for(i=0;i<len;i++){
-                printf("%c",string[i]);
-            }
-        }
+    }
     
     ak_libakrypt_destroy();
     return EXIT_SUCCESS;
