afe9b476ddefc0c61ee131a270b02b610a05f07e
oabelova <123364383+oabelova@users.noreply.github.com>
2023-02-07 02:11:44 +0300

    Update example-28.c

--- examples/faq/example-28.c
+++ examples/faq/example-28.c
@@ -4,25 +4,26 @@
 
 /*Пример чтения пароля из консоли*/
 
-int main(){
+int main( void )
+{
     if( ak_libakrypt_create( NULL ) != ak_true ) {
         /* инициализация выполнена не успешно, следовательно, выходим из программы */
         ak_libakrypt_destroy();
         return EXIT_FAILURE;
     }
-        char string[256];
-        size_t len = 256;
+    char string[256];
+    size_t len = 256;
 
-        if(ak_password_read(string,len)==ak_error_ok){
-            printf("Error with reading your password!\n");
+    if(ak_password_read(string,len)==ak_error_ok){
+        printf("Error with reading your password!\n");
+    }
+    else{
+        printf("Your password is: ");
+        int i=0;
+        for(i=0;i<len;i++){
+            printf("%c",string[i]);
         }
-        else{
-            printf("Your password is: ");
-            int i=0;
-            for(i=0;i<len;i++){
-                printf("%c",string[i]);
-            }
-        }
+    }
     
     ak_libakrypt_destroy();
     return EXIT_SUCCESS;
