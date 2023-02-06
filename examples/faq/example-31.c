b26e6883e7305620e152c4db319f6bed6ab3724e
oabelova <123364383+oabelova@users.noreply.github.com>
2023-02-07 02:14:13 +0300

    Update example-31.c

--- examples/faq/example-31.c
+++ examples/faq/example-31.c
@@ -3,24 +3,25 @@
 #include <libakrypt.h>
 
 /*Пример вычисления 32-битной контрольной суммы с помощью алгоритма Флетчера*/
-int main(){
-    
+int main( void )
+{
     if( ak_libakrypt_create( NULL ) != ak_true ) {
         /* инициализация выполнена не успешно, следовательно, выходим из программы */
         ak_libakrypt_destroy();
         return EXIT_FAILURE;
     }
-        ak_const_pointer data = "abcde";
-        ak_uint32 *res;
-        res = (ak_uint32*)malloc(1);
-        res[0]=0;
-        int result = ak_ptr_fletcher32(data,5,res);
+    
+    ak_const_pointer data = "abcde";
+    ak_uint32 *res;
+    res = (ak_uint32*)malloc(1);
+    res[0]=0;
+    int result = ak_ptr_fletcher32(data,5,res);
 
-        int i=0;
+    int i=0;
 
-        for(i=0;i<1;i++){
-            printf("%d \n %d",res[i],result);
-        }
+    for(i=0;i<1;i++){
+        printf("%d \n %d",res[i],result);
+    }
     
     ak_libakrypt_destroy();
     return EXIT_SUCCESS;
